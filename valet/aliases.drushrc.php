efca5b8a4e5f4fdd8850ad00affbe5f45ad28b17
James Williams <james.williams@computerminds.co.uk>
2020-04-28 12:00:21 +0100

    Fix drush aliases for valet

--- valet/aliases.drushrc.php
+++ valet/aliases.drushrc.php
@@ -2,13 +2,13 @@
 
 $aliases = array();
 $home_dir = drush_server_home();
-$sites_dir = '/sites';
+$sites_subdir = '/sites';
 $candidate_subdirs = array(
   '/webroot',
   '/web',
   '/docroot',
 );
-$dir_handle = new DirectoryIterator($home_dir . $sites_dir);
+$dir_handle = new DirectoryIterator($home_dir . $sites_subdir);
 
 while ($dir_handle->valid()) {
   if ($dir_handle->isDir() && !$dir_handle->isDot()) {
@@ -24,7 +24,8 @@ while ($dir_handle->valid()) {
     if (file_exists($dir_handle->getPathname() . $prefix . '/sites/default/default.settings.php') || file_exists($dir_handle->getPathname() . $prefix . '/sites/default/settings.php')) {
       $basename = $dir_handle->getBasename();
       $aliases[$basename] = array(
-        'uri' => 'https://' . $basename,
+        'uri' => 'https://' . $basename . '.test',
+        'root' => $home_dir . $sites_subdir . '/' . $basename . $prefix,
       );
     }
   }
